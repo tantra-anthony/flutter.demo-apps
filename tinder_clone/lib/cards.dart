87fe6eed581dacfa988c6695e4ad5bed6e2d0694
tantra-anthony <tantra.anthony@gmail.com>
2018-08-18 17:18:39 +0800

    added animation for card swiping and snapping back

--- tinder_clone/lib/cards.dart
+++ tinder_clone/lib/cards.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:fluttery/layout.dart';
 import 'package:tinder_clone/photos.dart';
@@ -8,7 +10,85 @@ class DraggableCard extends StatefulWidget {
   _DraggableCardState createState() => _DraggableCardState();
 }
 
-class _DraggableCardState extends State<DraggableCard> {
+class _DraggableCardState extends State<DraggableCard> with TickerProviderStateMixin {
+
+  Offset cardOffset = Offset(0.0, 0.0);
+  Offset dragStart;
+  Offset dragPosition;
+  Offset slideBackStart;
+  AnimationController slideBackAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    slideBackAnimation = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this
+    )
+    ..addListener(() => setState(() {
+      cardOffset = Offset.lerp(
+        slideBackStart, 
+        Offset(0.0, 0.0), 
+        Curves.elasticOut.transform(slideBackAnimation.value)
+      );
+    }))
+    ..addStatusListener((AnimationStatus status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+                      dragStart = null;
+                      slideBackStart = null;
+                      dragPosition = null;
+                    });
+        }
+    });
+    
+  }
+
+  @override
+  void dispose() {
+    slideBackAnimation.dispose();
+    super.dispose();
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    slideBackStart = cardOffset;
+    slideBackAnimation.forward(from: 0.0);
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    setState(() {
+          dragPosition = details.globalPosition;
+          cardOffset = dragPosition - dragStart;
+        });
+  }
+
+  void _onPanStart(DragStartDetails details) {
+    dragStart = details.globalPosition;
+
+    // handle when user drags during animation
+    if (slideBackAnimation.isAnimating) {
+      slideBackAnimation.stop(canceled: true);
+    }
+  }
+
+  double _rotation(Rect dragBounds) {
+    // to prevent from rotating too much
+    if (dragStart != null) {
+      final rotationCornerMultiplier = dragStart.dy >= dragBounds.top + (dragBounds.height / 2) ? -1 : 1;
+      return (pi / 8) * (cardOffset.dx / dragBounds.width) * rotationCornerMultiplier;
+    } else {
+      return 0.0;
+    }
+  }
+
+  Offset _rotationOrigin(Rect dragBounds) {
+    if (dragStart != null) {
+      return dragStart - dragBounds.topLeft;
+    } else {
+      return Offset(0.0, 0.0); 
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnchoredOverlay( // from fluttery package
@@ -18,16 +98,27 @@ class _DraggableCardState extends State<DraggableCard> {
         // build cards here
         return CenterAbout( // any child will be centred about this position
           position: anchor,
-          child: Container(
-            width: anchorBounds.width,
-            height: anchorBounds.height, // the overlay covers whole page
-            padding: EdgeInsets.all(16.0),
-            child: ProfileCard(),
+          child: Transform(
+            transform: Matrix4.translationValues(cardOffset.dx, cardOffset.dy, 0.0)
+              ..rotateZ(_rotation(anchorBounds)),
+            origin: _rotationOrigin(anchorBounds),
+            child: Container(
+              width: anchorBounds.width,
+              height: anchorBounds.height, // the overlay covers whole page
+              padding: EdgeInsets.all(16.0),
+              child: GestureDetector(
+                child: ProfileCard(),
+                onPanStart: _onPanStart,
+                onPanUpdate: _onPanUpdate,
+                onPanEnd: _onPanEnd,
+              ),
+            ),
           )
         );
       },
     );
   }
+
 }
 
 class ProfileCard extends StatefulWidget {
